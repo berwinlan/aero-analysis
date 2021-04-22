e7042542ef5c3b1cfb4d2ab4227c16b3b3085c33
Berwin Lan <blan@olin.edu>
2021-04-22 19:20:31 -0400

    idk what i did

---
+++ main.py
@@ -0,0 +1,4 @@
+import tail_sizing
+
+tail_sizing.vertical_tail_sizing()
+tail_sizing.horizontal_tail_sizing()
--- tail_sizing.py
+++ tail_sizing.py
@@ -1,26 +1,23 @@
 def vertical_tail_sizing():
-    L_v = float(input("Enter the vertical distance of tail to CG of aircraft (L_v):"))
-    S_v = float(input("Enter the trapezoidal area of the vertical tail (S_v):"))
-    S_w = float(input("Enter the wing area (S_w):"))
-    mac = float(input("Enter the MAC (mean aerodynamic chord):"))
-
     while True:
+        L_v = float(input("Enter the vertical distance of tail to CG of aircraft (L_v):"))
+        S_v = float(input("Enter the trapezoidal area of the vertical tail (S_v):"))
+        S_w = float(input("Enter the wing area (S_w):"))
+        mac = float(input("Enter the MAC (mean aerodynamic chord):"))
+
         V_v = (L_v * S_v) / (S_w * mac)
+        
         if input(f"V_v = {V_v}; press Y to continue or any key to exit") != "Y":
             break
 
-        S_v = float(input("Enter the trapezoidal area of the vertical tail (S_v):"))
-
 def horizontal_tail_sizing():
-    L_h = float(input("Enter the vertical distance of tail to CG of aircraft (L_v):"))
-    S_h = float(input("Enter the trapezoidal area of the horizontal tail (S_h):"))
-    S_w = float(input("Enter the wing area (S_w):"))
-    mac = float(input("Enter the MAC (mean aerodynamic chord):"))
-
-
     while True:
+        L_h = float(input("Enter the vertical distance of tail to CG of aircraft (L_v):"))
+        S_h = float(input("Enter the trapezoidal area of the horizontal tail (S_h):"))
+        S_w = float(input("Enter the wing area (S_w):"))
+        mac = float(input("Enter the MAC (mean aerodynamic chord):"))
+
         V_h = (L_h * S_h) / (S_w * mac)
+
         if input(f"V_h = {V_h}; press Y to continue or any key to exit") != "Y":
             break
-
-        S_h = float(input("Enter the trapezoidal area of the horizontal tail (S_h):"))
